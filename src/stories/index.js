18912e7cca062fa426cf4c105da5e264c42ae14e
Yaniv Ran <yran@appcolony.ca>
2017-05-31 10:07:02 -0700

    updated storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { storiesOf, action } from '@kadira/storybook'
+import { storiesOf, action } from '@storybook/react'
 import Card from '../client/components/card'
 import CompactCard from '../client/components/card/compact'
 
