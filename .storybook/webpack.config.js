81d1920376d96330c986ea72fde2f6553bc73def
Yaniv Ran <yran@appcolony.ca>
2017-09-11 21:05:29 -0700

    updates

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,39 +1,45 @@
 const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+// postcss
+const cssnext = require('postcss-cssnext');
+const postcssPartialImport = require('postcss-partial-import');
+const postcssMixins = require('postcss-mixins');
+const postcssSimpleVars = require('postcss-simple-vars');
+
+const PATHS = require('../paths');
 
 module.exports = {
-  module: {
-    loaders: [{ test: /bootstrap[\/\\]dist[\/\\]js[\/\\]umd[\/\\]/, loader: 'imports?jQuery=jquery' }],
-    rules: [
-      {
-        test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"],
-
-        include: path.resolve(__dirname, '../')
-      }, 
-      
-    ]
-  }, 
-  plugins: [
-      new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-        "window.jQuery": "jquery",
-        Tether: "tether",
-        "window.Tether": "tether",
-        Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
-        Button: "exports-loader?Button!bootstrap/js/dist/button",
-        Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
-        Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
-        Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
-        Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
-        Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
-        Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
-        Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
-        Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-        Util: "exports-loader?Util!bootstrap/js/dist/util",
-      })
+  resolve: {
+    root: [
+      PATHS.app,
+      path.resolve(__dirname, '../node_modules'),
     ],
-}
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.(png|woff|woff2|svg|ttf|eot|jpg)$/,
+        loader: 'file',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css?modules=1!postcss',
+      },
+    ],
+  },
+
+  postcss(instance) {
+    return [
+      postcssPartialImport({
+        dirs: [
+          PATHS.app,
+        ],
+        addDependencyTo: instance,
+      }),
+      postcssMixins(),
+      postcssSimpleVars(),
+      cssnext(),
+    ];
+  },
+};
